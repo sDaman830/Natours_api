b8d906a88394c5017ea3b753ffe0bbe2db16aa69
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-08-03 19:24:04 +0530

    corrected routing

--- app.js
+++ app.js
@@ -21,11 +21,35 @@ app.get('/api/v1/tours', (req, res) => {
         }
     })
 })
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tour: tour
+        }
+    })
+})
 
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
+    const newID = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ if: newId }, req.body)
+    tours.push(newTour);
+    fs.writeFile(`/Users/damanpreetsinghghatoura/Desktop/Natours_api/dev-data/data/tours-simple1.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: `success`,
+            data: {
+                tours: newTour
 
-    res.send('Done');
+            }
+        })
+
+    })
+
 })
 app.listen(port, () => {
     console.log('Running')
